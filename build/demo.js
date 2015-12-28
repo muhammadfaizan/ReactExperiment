2d1281d9eafc0609305489d90a181fcba457fc2a
muhammadfaizan <muhammad.faizan01@live.com>
2015-12-28 20:36:21 +0500

    made function out of create class

--- build/demo.js
+++ build/demo.js
@@ -1,37 +1,33 @@
 /**
  * Created by VD on 12/28/2015.
  */
-var Demo = React.createClass({
-    displayName: 'Demo',
-
-    render: function () {
-        var radioOption = [{ value: 'newspaper', label: 'Newspaper' }, { value: 'radio', label: 'Radio' }, { value: 'search', label: 'Seach Engine' }, { value: 'social', label: 'Social Media' }];
-        return React.createElement(
-            'div',
-            { className: 'container' },
+function Demo() {
+    var radioOption = [{ value: 'newspaper', label: 'Newspaper' }, { value: 'radio', label: 'Radio' }, { value: 'search', label: 'Seach Engine' }, { value: 'social', label: 'Social Media' }];
+    return React.createElement(
+        'div',
+        { className: 'container' },
+        React.createElement(
+            'h1',
+            null,
+            'React.js Radio Group Demo'
+        ),
+        React.createElement(
+            'form',
+            null,
             React.createElement(
-                'h1',
-                null,
-                'React.js Radio Group Demo'
+                'p',
+                { className: 'h3' },
+                'How did you hear about us?'
             ),
+            React.createElement(RadioOptionGroup, { name: 'referrer',
+                other: true,
+                options: radioOption
+            }),
             React.createElement(
-                'form',
+                'p',
                 null,
-                React.createElement(
-                    'p',
-                    { className: 'h3' },
-                    'How did you hear about us?'
-                ),
-                React.createElement(RadioOptionGroup, { name: 'referrer',
-                    other: true,
-                    options: radioOption
-                }),
-                React.createElement(
-                    'p',
-                    null,
-                    React.createElement('input', { type: 'submit' })
-                )
+                React.createElement('input', { type: 'submit' })
             )
-        );
-    }
-});
+        )
+    );
+}
